22425fd3263bf2860506d6d5d6581bf09a49c9ff
AOlefirenko <olef_and@ukr.net>
2021-04-17 19:58:43 -0400

    ico test

--- contracts/test/sample-test.js
+++ contracts/test/sample-test.js
@@ -2,19 +2,6 @@ const { expect } = require("chai");
 const hre = require("hardhat");
 const web3 = require("web3");
 
-// describe("Greeter", function () {
-//   it("Should return the new greeting once it's changed", async function () {
-//     const Greeter = await ethers.getContractFactory("Greeter");
-//     const greeter = await Greeter.deploy("Hello, world!");
-
-//     await greeter.deployed();
-//     expect(await greeter.greet()).to.equal("Hello, world!");
-// await hre.run("compile");
-//     await greeter.setGreeting("Hola, mundo!");
-//     expect(await greeter.greet()).to.equal("Hola, mundo!");
-//   });
-// });
-
 describe("Deploy gov token and assert owner's balance", () => {
   it("should deploy and show balance", async () => {
     const totalSupply = 100000;
@@ -31,7 +18,7 @@ describe("Deploy gov token and assert owner's balance", () => {
 
     let bal = await token.balanceOf(owner.address)
     expect(bal).to.eq(1000)
-    expect(await token.totalSupply()).to.equal(bal);
+    expect(await token.totalSupply()).to.equal(1000);
   });
 
   it("should deploy", async () => {
@@ -48,10 +35,55 @@ describe("Deploy gov token and assert owner's balance", () => {
     await token.deployed();
     await token.mint(owner.address, 1000) // 1e-15
 
-    const bal = await token.totalSupply()
-
     await token.transfer(addr1.address, 50);
     expect(await token.balanceOf(addr1.address)).to.equal(50);
+  });
+
+  it("should deploy", async () => {
+    const totalSupply = 100000;
+    const [owner, addr1] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    const ICO = await ethers.getContractFactory("ICO");
+
+    const token = await Token.deploy(
+      "Governance Test token",
+      "gTST",
+      totalSupply
+    );
+
+    await token.deployed();
+    await token.mint(owner.address, 1000) // 1e-15
+
+    const fakeDai = await Token.deploy(
+      "Not dai but close",
+      "DAI",
+      totalSupply
+    );
+
+    await fakeDai.deployed()
+    await fakeDai.mint(owner.address, 1000)
+
+    const ammm = await web3.utils.toWei("2", "milli");
+    const ico = await ICO.deploy(
+      token.address,
+      5,
+      ammm,
+      totalSupply, //_availableTokens for the ICO. can be less than maxTotalSupply
+      20, //_minPurchase (in DAI)
+      50000,
+      fakeDai.address
+    );
+
+    await token.deployed();
+    await ico.deployed();
+    await token.updateAdmin(ico.address);
+    await ico.start();
+    // console.log(ico.address)
+
+    await fakeDai.increaseAllowance(owner.address, 20)
+    console.log((await fakeDai.allowance(ico.address, owner.address)).toNumber())
+    
+    console.log(await ico.connect(owner).buy(2)) 
 
   });
 });
